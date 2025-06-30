97f71b7cab927450ec4bff1fd357fb8f866745fa
LuccaL <106239988+AlephLLTR@users.noreply.github.com>
2025-06-30 16:45:04 -0300

    Iniciando a implementação do DepartmentDaoJDBC

---
+++ src/application/App2.java
@@ -0,0 +1,13 @@
+package application;
+
+public class App2 {
+    public static void main(String[] args) {
+        System.out.println("=== TESTE 1: findByID ===");
+        System.out.println("\n=== TESTE 2: findByDepartment ===");
+        System.out.println("\n=== TESTE 3: findAll ===");
+        System.out.println("\n=== TESTE 4: insert ===");
+        System.out.println("\n=== TESTE 5: update ===");
+        System.out.println("\n=== TESTE 6: delete ===");
+
+    }
+}
